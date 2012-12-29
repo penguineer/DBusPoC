9b962a179ada5f3fe7d99937eefae427e1079882
Stefan Haun <stefan.haun@ovgu.de>
2012-12-29 15:02:18 +0100

    Basic Maven Projekt

---
+++ DbusPoC/src/main/java/com/penguineering/dbuspoc/DBusPoC.java
@@ -0,0 +1,16 @@
+package com.penguineering.dbuspoc;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class DBusPoC {
+	private static Log log = LogFactory.getLog(DBusPoC.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		log.info("Hallo Welt!");
+	}
+
+}
